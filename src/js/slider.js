c18a0ec814b47351a142210a73e2d8262fee4877
Sudnikevych <sudnikevych@gmail.com>
2022-07-09 16:47:26 +0300

    add slider

---
+++ src/js/slider.js
@@ -0,0 +1,11 @@
+$('.slider-list').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true,
+  arrows: true,
+  nextArrow: '.next',
+  prevArrow: '.prev',
+  autoplay: true,
+});
